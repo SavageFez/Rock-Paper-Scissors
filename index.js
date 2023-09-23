73cd42fba42608c187ff57447bab0ad16b468d76
SavageFez <jmaston13@gmail.com>
2023-09-23 14:24:57 +0900

    Completed first prototype of index.js. Base code appears to work correctly. Need to add input validation and error handling

--- index.js
+++ index.js
@@ -7,41 +7,79 @@ function getComputerChoice() {
     switch (randomNumber) {
         case 1:
             //console.log("Computer Choice: Rock");
-            return 1;
+            return "ROCK";
             break;
         case 2:
             //console.log("Computer Choice: Paper");
-            return 2;
+            return "PAPER";
             break;
         case 3:
             //console.log("Computer Choice: Scissors");
-            return 3;
+            return "SCISSORS";
             break;
     }
 }
 
 
 function getPlayerChoice() {
-    return null;
+    let choice = prompt("Select Rock, Paper, or Scissors").toUpperCase();
+    return choice;
 }
 
-function playRound() {
-    return null;
+function playRound(cpu, player) {
+    cpu = getComputerChoice();
+    player = getPlayerChoice();
+
+
+    console.log("Computer Choice: " + cpu);
+    console.log("Player Choice: " + player);
+
+    if (cpu == "ROCK" && player == "ROCK") {
+        console.log("DRAW!");
+    } else if (cpu == "ROCK" && player == "PAPER") {
+        console.log("YOU WIN! PAPER BEATS ROCK!");
+        return true;
+    } else if (cpu == "ROCK" && player == "SCISSORS") {
+        console.log("YOU LOSE! ROCK BEATS SCISSORS!");
+        return false;
+    } else if (cpu == "PAPER" && player == "PAPER") {
+        console.log("DRAW!");
+    } else if (cpu == "PAPER" && player == "ROCK") {
+        console.log("YOU LOSE! PAPER BEATS ROCK!");
+        return false;
+    } else if (cpu == "PAPER" && player == "SCISSORS") {
+        console.log("YOU WIN! SCISSORS BEATS PAPER!");
+        return true;
+    } else if (cpu == "SCISSORS" && player == "SCISSORS") {
+        console.log("DRAW!");
+    } else if (cpu == "SCISSORS" && player == "PAPER") {
+        console.log("YOU LOSE! SCISSORS BEATS PAPER!");
+        return false;
+    } else if (cpu == "SCISSORS" && player == "ROCK") {
+        console.log("YOU WIN! ROCK BEATS SCISSORS!");
+        return true;
+    } else {
+        return console.error("Failed at playRound");
+    }
 }
 
+let cpuScore = 0;
+let playerScore = 0;
+let drawScore = 0;
 
+for (let i = 5; i > 0; i--) {
+    
+    result = playRound();
 
-// console.log(getComputerChoice());
+    if (result == false) {
+        cpuScore += 1;
+    } else if (result == true) {
+        playerScore += 1;
+    } else {
+        drawScore += 1;
+    }
+}
 
-/*
-game(1, 1); // draw 
-game(1, 2); // win
-game(1, 3); // lose
-game(2, 1); // lose 
-game(2, 2); // draw
-game(2, 3); // win
-game(3, 1); // lose
-game(3, 2); // win
-game(3, 3); // draw
-*/
+console.log("Computer Score: " + cpuScore + " | " + "Player Score: " + playerScore + " | " + "Draws:" + drawScore);
+
 
